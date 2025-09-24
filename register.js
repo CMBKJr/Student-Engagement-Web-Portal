48f1c1febbfeef988ceff8a25ef6e067cccc8484
Dawson Mathews <79239282+CMBKJr@users.noreply.github.com>
2025-09-24 18:14:39 -0400

    Add files via upload

---
+++ register.js
@@ -0,0 +1,7 @@
+function Register() {
+    const Fname = document.getElementById("Fname").value;
+    const Lname = document.getElementById("Lname").value;
+    const Email = document.getElementById("Email").value;
+    const Pwd = document.getElementById("Pwd").value;
+    
+}
