7319f25e4b64d66311252bb83d8e9d3eb79e2700
Ekene Ezeobi <ezeobiekene7@gmail.com>
2025-11-21 17:19:28 -0500

    removed styles from and loaded events page

--- frontend/src/pages/EventSite.jsx
+++ frontend/src/pages/EventSite.jsx
@@ -1,47 +1,76 @@
-import Notifications from '../components/Notifications';
-import Events from '../components/Events'
-import { useEventContext } from '../contexts/EventContext';
-function EventSite () {
-    //const {isReg, addToReg, removeFromReg} = useEventContext()
-    const events = [
-        {id: 1, title: "Hackathon", description:"stuff stuff stuff", location:"Marietta", date: "September 9th", time: "6:00 pm"},
-         {id: 2, title: "Hide N' Seek", description:"stuff stuff stuff", location:"Marietta", date: "September 22nd", time: "10:00 pm"},
-         {id: 3, title:'Event 3', description:'eventdescr', location:"eventloca", date:"eventdate", time:"eventtime"}
-        ]
-        const notification = [
-            {id:1, message:"Three days left to rsvp to the upcoming Hackathon event!"},
-            {id: 2, message:"notification two"},
-            {id: 3, message:"notification three"},
-            {id:4, message:"notification four"}
-        ]
+import Notifications from "../components/Notifications";
+import Events from "../components/Events";
+import eventServices from "../api/eventServices";
+import { useEventContext } from "../contexts/EventContext";
+import { useEffect, useState } from "react";
 
-return (
+function EventSite() {
+  //const {isReg, addToReg, removeFromReg} = useEventContext()
+  const [events, setEvents] = useState([]);
+
+  const notification = [
+    {
+      id: 1,
+      message: "Three days left to rsvp to the upcoming Hackathon event!",
+    },
+    { id: 2, message: "notification two" },
+    { id: 3, message: "notification three" },
+    { id: 4, message: "notification four" },
+  ];
+
+  const fetchData = async () => {
+    try {
+      const res = await eventServices.getAll();
+
+      if (res.data) {
+        setEvents(res.data);
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
     <>
-    <div className = "whiteBack">
-    <nav style={{border: '1px solid lightgray', height:'120px'}}>
-    <p style= {{marginRight:'1000px', fontSize:'20px', marginBottom: '35px', fontWeight: 'bold', letterSpacing: '2px', color: 'black'}}>Student Engagement Portal</p>
-       <p style={{marginBottom: '80px', marginLeft:'1020px'}}>
-        Home&nbsp;&nbsp;&nbsp;Events&nbsp;&nbsp;&nbsp;Attendance&nbsp;&nbsp;&nbsp;Profile
-        </p>
-            <p style= {{marginLeft:'1100px', fontSize:'25px'}}>Notifications</p>
-        {notification.map(notification => <Notifications notification={notification}
-        key={notification.id}/>)}
-    </nav>
-    <div className='Event-Wall' style={{}}>
-        <h2 style={{marginRight: '1070px'}}>Registered Events</h2>   
-         <p>No registered events yet!</p>
-        <h2 style={{marginRight: '1070px'}}>Upcoming Events</h2>
-        {events.map(events => <Events events={events}
-        key={events.id}/>)}
-        <h2 style={{marginRight: '1195px'}}>Events</h2>
-            <Events events = {{id: 4, title:'Event 4', description:'eventdescr', location:"eventloca", date:"eventdate", time:"eventtime"}}></Events>
-                <button style ={{marginRight:"150px", color: 'white', backgroundColor:'black'}}>View all Events</button>
-</div>
-<footer style={{textAlign:'left', color:'white', backgroundColor:'black'}}>
-
-</footer>
-    </div>
+      <div className="whiteBack">
+        <nav>
+          <p>Student Engagement Portal</p>
+          <p>
+            Home&nbsp;&nbsp;&nbsp;Events&nbsp;&nbsp;&nbsp;Attendance&nbsp;&nbsp;&nbsp;Profile
+          </p>
+          <p>Notifications</p>
+          {notification.map((notification) => (
+            <Notifications notification={notification} key={notification.id} />
+          ))}
+        </nav>
+        <div className="Event-Wall">
+          <h2>Registered Events</h2>
+          <p>No registered events yet!</p>
+          <h2>Upcoming Events</h2>
+          {events.map((events, index) => (
+            <Events events={events} key={events._id} />
+          ))}
+          <h2>Events</h2>
+          <Events
+            events={{
+              id: 4,
+              title: "Event 4",
+              description: "eventdescr",
+              location: "eventloca",
+              date: "eventdate",
+              time: "eventtime",
+            }}
+          ></Events>
+          <button>View all Events</button>
+        </div>
+        <footer></footer>
+      </div>
     </>
-)
+  );
 }
 export default EventSite;
