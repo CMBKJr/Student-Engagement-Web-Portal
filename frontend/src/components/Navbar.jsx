0b8dbc2b7c42fe02932444701f04fba2271f4bbc
Ekene Ezeobi <ezeobiekene7@gmail.com>
2025-11-23 22:39:51 -0500

    Moved logout to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const picture = localStorage.getItem('LoggedInPicture')
+  const [profileClick, setProfileClick] = useState(false);
+  const picture = localStorage.getItem("LoggedInPicture");
+
+  const handleProfileClick = (event) => {
+    event.preventDefault();
+    setProfileClick(!profileClick);
+  };
+
   return (
     <div>
       <nav className="event-nav navbar">
@@ -24,14 +30,56 @@ const Navbar = () => {
           <li>
             <Link to={"/milestone"}>Milestone</Link>
           </li>
-          <li>
-            <Link to={"/profile"}>Profile</Link>
+          <li className="profile-wrapper" onClick={handleProfileClick}>
+            <img className="display-picture" src={picture} alt="" />
           </li>
-         
-        </ul> 
+        </ul>
         {/* <img src={picture} alt="" /> */}
       </nav>
+      {profileClick && <ProfileIconMore />}
     </div>
+  );
+};
+
+const ProfileIconMore = () => {
+  const [display, setDisplay] = useState(false);
+  const navigate = useNavigate()
+  const logout = async () => {
+    try {
+      // const res = await userServices.logout();
+
+      localStorage.removeItem("LoggedInEmail");
+      localStorage.removeItem("LoggedInFirstName");
+      localStorage.removeItem("LoggedInLastName");
+      localStorage.removeItem("LoggedInID");
+      localStorage.removeItem("LoggedInPicture");
+      localStorage.removeItem("LoggedInRole");
+
+      console.log("Logout Successfull");
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDisplay = (event) => {
+    event.preventDefault();
+    setDisplay(!display);
+  };
+
+  return (
+    <>
+      <div className="more-profile">
+        <span onClick={handleDisplay} className="ui-nav">Update Image</span>
+
+        {display && <input className="input-nav" type="file" name="" id="" />}
+
+        <button onClick={logout} className="signout">
+          Sign Out
+        </button>
+      </div>
+    </>
   );
 };
 
