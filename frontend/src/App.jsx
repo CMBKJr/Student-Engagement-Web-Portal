6d197ae949d9ace22d7cf4211aae69644a7d96f0
Dawson Mathews <79239282+CMBKJr@users.noreply.github.com>
2025-11-25 11:21:21 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import MainLogin from "./pages/MainLogin";
-import AdminLogin from "./pages/AdminLogin";
 import SignUp from "./pages/SignUp";
 import ForgotPSW from "./pages/ForgotPSW";
 import Milestone from "./pages/Milestone";
@@ -30,7 +29,6 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLogin />} />
         <Route path="/login" element={<MainLogin />} />
-        {/* <Route path="/admin" element={<AdminLogin />} /> */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPSW />} />
         <Route path="/milestone" element={<Milestone />} />
