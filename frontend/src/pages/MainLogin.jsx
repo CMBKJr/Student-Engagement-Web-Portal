75bc80d16e9a03a5ce52b6e0658fb6b2f468f1d9
Dawson Mathews <79239282+CMBKJr@users.noreply.github.com>
2025-11-25 11:20:23 -0500

    Update MainLogin.jsx

--- frontend/src/pages/MainLogin.jsx
+++ frontend/src/pages/MainLogin.jsx
@@ -51,35 +51,6 @@ const MainLogin = () => {
       console.log(error.message);
     }
 
-    // try {
-    //   const response = await axios.post(
-    //     LOGIN_URL,
-    //     JSON.stringify({ email, password }),
-    //     {
-    //       headers: { "Content-Type": "application/json" },
-    //       withCredentials: true,
-    //     }
-    //   );
-    //   console.log(JSON.stringify(response?.data));
-
-    //   const accessToken = response?.data?.accessToken;
-    //   const roles = response?.data?.roles;
-    //   setAuth({ email, password, roles, accessToken });
-    //   setEmail("");
-    //   setPassword("");
-    //   navigate("/forgot-password");
-    // } catch (err) {
-    //   if (!err?.response) {
-    //     setErrMsg("No Server Response");
-    //   } else if (err.response?.status === 400) {
-    //     setErrMsg("Missing Username or Password");
-    //   } else if (err.response?.status === 401) {
-    //     setErrMsg("Unauthorized");
-    //   } else {
-    //     setErrMsg("Login Failed");
-    //   }
-    //   errRef.current.focus();
-    // }
   };
 
   return (
